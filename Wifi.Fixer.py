2cc6d7d5fee93dd6e6c41c367af910faeb6b3c89
jackshipley12 <jackattack103@yahoo.com>
2021-03-17 08:59:59 -0400

    os coommand script

--- Wifi.Fixer.py
+++ Wifi.Fixer.py
@@ -1,2 +1,6 @@
 # we need to know everything
 # terminal cod: sudo service network-manager restart
+
+import os
+script = "sudo service network-manager restart"
+os.system("sudo service network-manager restart")
