078772eee1e6723c115f656de34e1477e3d3955b
jackshipley12 <jackattack103@yahoo.com>
2021-03-18 09:05:33 -0400

    user interface

--- Wifi.Fixer.py
+++ Wifi.Fixer.py
@@ -1,5 +1,14 @@
 # we need to know everything
 # terminal cod: sudo service network-manager restart
 
-import os
-os.system("sudo service network-manager restart")
+#import os
+#os.system("sudo service network-manager restart")
+
+connection_status = input("Do you want to rest internet? Please answer with 'Y' or 'N': ")
+print(connection_status)
+
+user_response = input()
+
+if user_respone == 'Y':
+    import os
+    os.system("sudo service network-manager restart")
